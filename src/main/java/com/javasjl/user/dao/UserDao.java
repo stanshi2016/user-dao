7e3c02d669f98ef318ad38886a226c0ee51f8fab
stanshi2016 <hmsjl1989@hotmail.com>
2017-08-21 01:07:25 +0800

    Initial commit

---
+++ src/main/java/com/javasjl/user/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.javasjl.user.dao;
+
+import com.javasjl.user.entity.User;
+
+/**
+ * 用户DAO接口
+ * @author stanshi
+ *
+ */
+public interface UserDao {
+	
+	public User login(User user);
+
+}
